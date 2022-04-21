cc84a6dab75a3b078b8883a4677edcddb70cd16d
daniel or abayev <dan5204@gmail.com>
2022-04-21 12:47:12 +0300

    Fix the documentation and use more informative variable names.

--- fast_and_reliable.py
+++ fast_and_reliable.py
@@ -4,31 +4,31 @@ from typing import IO
 
 def fast_and_reliable(file: IO[str]):
     """
-    This function receive file to read and read all the words in it to list and set, then check the average time take
-    to find the word in the list/set.
+    This function receives a file to read, and reads all of the words from inside it into a list and into a set.
+    Then, it checks the average time it takes to find a given word in the list/set.
     :param file: The file to read from.
     """
     words_list, words_set = [], set
     data = file.read()
     words_list = data.split()
     words_set = set(words_list)
-    print("search in set take " + average_runtime(words_set).__str__())
-    print("search in list take " + average_runtime(words_list).__str__())
+    print("A search in set takes " + average_runtime(words_set).__str__())
+    print("A search in list takes " + average_runtime(words_list).__str__())
 
 
 def average_runtime(list_or_set: list / set) -> float:
     """
-    This function receive list or set of words, then check the average time take to find the word zwitterion in the
-    set/list.
+    This function receives list or set of words, then checks the average time it takes to find the word zwitterion in
+    the set/list.
     :param list_or_set: List or set of words to search in.
-    :return: The time it take to search in the data structure in average.
+    :return: The time it takes to search in the data structure in average.
     """
     sum_time = 0
     for check_number in range(1000):
-        start = time.time()
+        time_before_start_find = time.time()
         "zwitterion" in list_or_set
-        end = time.time()
-        sum_time += end - start
+        time_after_end_find = time.time()
+        sum_time += time_after_end_find - time_before_start_find
     return sum_time / 1000
 
 
